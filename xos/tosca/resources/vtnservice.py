9ddeac76a08b23c846f44d3ea81dbccfe866b6fe
Zack Williams <zdw@cs.arizona.edu>
2016-10-05 16:23:59 -0700

    remote unecessary includes
    Change-Id: Ifbbd0f5208f8934ba6d1c3ff76488bfde4796f3f

--- xos/tosca/resources/vtnservice.py
+++ xos/tosca/resources/vtnservice.py
@@ -1,12 +1,4 @@
-import os
-import pdb
-import sys
-import tempfile
-sys.path.append("/opt/tosca")
-from translator.toscalib.tosca_template import ToscaTemplate
-
 from services.vtn.models import VTNService
-
 from service import XOSService
 
 class XOSVTNService(XOSService):
